7972d9749e084441eaa7199a193fe17fd7ce1691
jeremiasbustos02 <jeremias.e.bustos@gmail.com>
2026-05-27 03:05:09 -0300

    feat: logo y nombre

--- src/components/ui/BrandLogo.tsx
+++ src/components/ui/BrandLogo.tsx
@@ -2,10 +2,29 @@ type BrandLogoProps = {
   className?: string
 }
 
-export default function BrandLogo({ className = 'size-8' }: BrandLogoProps) {
+export default function BrandLogo({ className = 'h-8 w-auto' }: BrandLogoProps) {
   return (
-    <svg viewBox="0 0 256 256" className={`${className} fill-white`} aria-hidden="true">
-      <path d="M 0 128 C 70.692 128 128 185.308 128 256 L 64 256 C 64 220.654 35.346 192 0 192 Z M 256 192 C 220.654 192 192 220.654 192 256 L 128 256 C 128 185.308 185.308 128 256 128 Z M 128 0 C 128 70.692 70.692 128 0 128 L 0 64 C 35.346 64 64 35.346 64 0 Z M 192 0 C 192 35.346 220.654 64 256 64 L 256 128 C 185.308 128 128 70.692 128 0 Z" />
+    <svg 
+      version="1.1" 
+      xmlns="http://www.w3.org/2000/svg" 
+      xmlnsXlink="http://www.w3.org/1999/xlink" 
+      x="0px" 
+      y="0px"
+      viewBox="165 47 670 906"
+      className={className} 
+      aria-hidden="true"
+      xmlSpace="preserve"
+    >
+      <g id="Capa_2">
+        <path 
+          id="Logo" 
+          fill="#FFFFFF" 
+          d="M834.48,550.78v190.61L503.5,952.59L165.52,740.65V566.23c137.48-82.33,274.95-164.65,412.44-246.98
+            c27.96-16.02,44.04-46.58,40.89-77.55c-3.56-35.04-31.07-64.29-67.34-70.65H279.96v209.73L165.52,452.9V47.41h486.85
+            c83.79,7.68,154.14,67.05,175.52,147.93c20.96,79.28-9.21,164.03-75.8,212.68c-123.76,74.45-348.39,148.9-472.15,223.34v44.16
+            l224.09,88.93l217.47-83.41v-62.92l-182.14-1.11l193.18-130.26L834.48,550.78z"
+        />
+      </g>
     </svg>
   )
 }
--- src/data/site.ts
+++ src/data/site.ts
@@ -10,7 +10,7 @@ export const fadeUp = {
 } as const
 
 export const SITE = {
-  brandName: 'milogo.',
+  brandName: 'buveg',
   videoBackground:
     'https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260508_064122_c4750c0e-7476-4b44-94a2-a85a65c63bf2.mp4',
   socialProofInitials: ['MS', 'LG', 'FM'] as const,
